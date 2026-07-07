74ffd88baeebd0688dc7123db5fbf90233f24cbd
lfz <lifangzheng@lejurobot.com>
2026-07-07 15:21:44 +0800

    style: 移除无用代码

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,3 @@
-import Flex from '@/ui/Flex'
-import { Button } from '@/ui/Button'
-
 export default function Home() {
-  return (
-    <>
-      <Flex
-        center
-        className='text-white'
-      >
-        <Button>复制</Button>
-      </Flex>
-    </>
-  )
+  return <>Home</>
 }
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,30 +1,37 @@
 import type { AppRouteObject } from './types'
 import { Lazy } from '@/components/LazyImport'
 import BasicGuard from './guards/BasicGuard'
+import Layout from '@/layout'
 
 const routes: AppRouteObject[] = [
   {
     element: <BasicGuard />,
     children: [
       {
-        path: '/',
-        element: Lazy(() => import('@/pages/home')),
-        meta: { title: '首页' },
+        element: <Layout />,
+        children: [
+          {
+            path: '/',
+            element: Lazy(() => import('@/pages/home')),
+            meta: { title: '首页' },
+          },
+
+          {
+            path: '/404',
+            element: Lazy(() => import('@/pages/404')),
+            meta: { title: '404' },
+          },
+          {
+            path: '*',
+            element: Lazy(() => import('@/pages/404')),
+            meta: { title: '404' },
+          },
+        ],
       },
       {
         path: '/login',
         element: Lazy(() => import('@/pages/login')),
         meta: { title: '登陆' },
-      },
-      {
-        path: '/404',
-        element: Lazy(() => import('@/pages/404')),
-        meta: { title: '404' },
-      },
-      {
-        path: '*',
-        element: Lazy(() => import('@/pages/404')),
-        meta: { title: '404' },
       },
     ],
   },
