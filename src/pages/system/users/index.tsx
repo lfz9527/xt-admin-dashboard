b89fcf712348543a38a503fb72d6ac09942ac513
lifangzheng <lifangzheng@lejurobot.com>
2026-08-22 14:05:09 +0800

    refactor: 简化 users 页面并移除 superpowers 报告跟踪
    - users 页面移除演示按钮与 Outlet，删除无用的 detail 子页
    - 将 .superpowers/sdd/task-3-report.md 移出 git 跟踪（该目录已被 .gitignore 忽略）

--- src/pages/system/users/index.tsx
+++ src/pages/system/users/index.tsx
@@ -1,21 +1,3 @@
-import { Outlet, useNavigate } from 'react-router'
-
 export default function Users() {
-  const navigate = useNavigate()
-
-  return (
-    <>
-      users
-      <button
-        type='button'
-        onClick={() => {
-          const id = Math.floor(Math.random() * 1000000)
-          navigate(`/system/users/${id}`)
-        }}
-      >
-        查询用户详情
-      </button>
-      <Outlet />
-    </>
-  )
+  return <>users</>
 }
